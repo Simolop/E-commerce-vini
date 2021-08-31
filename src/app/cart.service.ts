a8b52afa29547db866b9af649cbdeee8dc90740e
Ilaria <ilaria.galliano7@gmail.com>
2021-08-31 14:54:55 +0200

    add to cart

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -4,6 +4,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class CartService {
+  getItemEvent() {
+    throw new Error('Method not implemented.');
+  }
+  addItemEvent() {
+    throw new Error('Method not implemented.');
+  }
     
   constructor() { }
 }
